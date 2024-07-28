64d661d75896fcc871e96d64099b4d3abd56eede
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2024-07-27 20:33:56 -0500

    Update nodes.py

--- liesel_ptm/nodes.py
+++ liesel_ptm/nodes.py
@@ -86,6 +86,57 @@ class TransformationDist(lsl.Dist):
         return self
 
 
+class TransformationDistLogDeriv(lsl.Dist):
+    """A transformation-distribution node for a conditional transformation model."""
+
+    def __init__(
+        self,
+        transformed_variable: lsl.Calc,
+        log_transformation_derivative: lsl.Calc,
+        refdist: TFPDistribution,
+        _name: str = "",
+        _needs_seed: bool = False,
+    ):
+        super(lsl.Dist, self).__init__(
+            transformed_variable,
+            log_transformation_derivative,
+            _name=_name,
+            _needs_seed=_needs_seed,
+        )
+
+        self._per_obs = True
+        self.refdist = refdist
+        self._transformed_variable = transformed_variable
+        self._log_transformation_derivative = log_transformation_derivative
+
+    @property
+    def log_prob(self) -> Array:
+        """The log-probability of the distribution."""
+        return self.value
+
+    @property
+    def per_obs(self) -> bool:
+        """Whether the log-probability is stored per observation or summed up."""
+        return self._per_obs
+
+    @per_obs.setter
+    @no_model_setter
+    def per_obs(self, per_obs: bool):
+        self._per_obs = per_obs
+
+    def update(self) -> TransformationDist:
+        base_log_prob = self.refdist.log_prob(self._transformed_variable.value)
+        log_prob_adjustment = self._log_transformation_derivative.value
+        log_prob = jnp.add(base_log_prob, log_prob_adjustment)
+
+        if not self.per_obs and hasattr(log_prob, "sum"):
+            log_prob = log_prob.sum()
+
+        self._value = log_prob
+        self._outdated = False
+        return self
+
+
 def model_matrix(*args: Array, intercept: bool = True) -> Array:
     x = np.vstack(args)
     if intercept:
